d1f5001a4e30218f5905148e92ea03515277bc92
cgarcia <cgarcia@promax.es>
2024-02-09 14:54:50 +0100

    install tailwind

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="flex max-w-sm my-0 mx-auto p-6 rounded-lg bg-white shadow-xl">
+      <div className="flex-shrink-0 items-center">
+        <picture className="h-12 w-12">
+          <source srcSet={logo} type="image/webp" />
+          <img src={logo} className="h-12 w-12" alt="Logo" />
+        </picture>
+        <div className="ml-6 pt-1">
+          <h4 className="text-xl text-gray-900 leading-tight">ChitChat</h4>
+          <p className="text-base text-gray-600 leading-normal">You have a new message!</p>
+        </div>
+      </div>
     </div>
   );
 }
