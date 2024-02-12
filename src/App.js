2ff73918bd472f254cd832247ae46475548064a8
cgarcia <cgarcia@promax.es>
2024-02-12 14:54:44 +0100

    start with tailwind

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex max-w-sm my-0 mx-auto p-6 rounded-lg bg-white shadow-xl">
-      <div className="flex-shrink-0 items-center">
-        <picture className="h-12 w-12">
-          <source srcSet={logo} type="image/webp" />
-          <img src={logo} className="h-12 w-12" alt="Logo" />
-        </picture>
-        <div className="ml-6 pt-1">
-          <h4 className="text-xl text-gray-900 leading-tight">ChitChat</h4>
-          <p className="text-base text-gray-600 leading-normal">You have a new message!</p>
+    <header className="bg-blue-950 px-20 py-5 shadow">
+      <div className="flex h-16 items-center justify-between">
+        <a href="e" className="text-white hover:text-slate-500 focus:ring-2">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </a>
+        <a href="e" className="text-white">
+          <img src={logo} className="h-20 w-20" alt="Logo" />
+        </a>
+        <div>
+          <button type="button" className="text-black">
+            theme
+          </button>
+          <button type="button" className="text-black">
+            config
+          </button>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
 
---
+++ tailwind.readme.js
@@ -0,0 +1,9 @@
+/*
+  - List of Tailwind classes and how they are used
+    --> -bg-white: background-color-white https://tailwindcss.com/docs/customizing-colors
+    --> -m-2: margin-2 (2rem)
+    --> -p-2: padding-2 (2rem)
+    --> -shadow-xl: box-shadow-xl
+    --> -flex: display-flex
+    --> -max-w-sm: max-width-sm (24rem)
+*/
